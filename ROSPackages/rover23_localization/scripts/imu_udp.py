03fdb21e6bf7e6d4acdb79df8ec6653c9f8694ef
sarp <sarpsonal@gmail.com>
2025-08-26 20:02:54 +0300

    imu_udp güncelleme

--- ROSPackages/rover23_localization/scripts/imu_udp.py
+++ ROSPackages/rover23_localization/scripts/imu_udp.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 import socket
 import struct
@@ -5,14 +6,18 @@ import rospy
 from sensor_msgs.msg import Imu
 
 #--config--
-LOCAL_IP = "192.168.1.2" # IP to listen on
-LOCAL_PORT = 6300 # Port of the IP to listen on
+LOCAL_IP = "192.168.1.95" # IP to listen on
+LOCAL_PORT = 6102 # Port of the IP to listen on
+REMOTE_IP = "192.168.1.2" # IP to send messages to
+REMOTE_PORT = 6101 # Port to send messages to
 FRAME_ID = "imu_link" # the name of the coordinate frame of the imu
 
 class imu_udp():
     def __init__(self):
+        self.timestamp = [0,0]
         self.pub = rospy.Publisher("/imu/data", Imu, queue_size=10)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.settimeout(2.0)
 
         try:
             self.sock.bind((LOCAL_IP, LOCAL_PORT))
@@ -21,6 +26,18 @@ class imu_udp():
             rospy.loginfo(f"failed to bind socket: {e}")
             exit(1)
         
+    def request_data(self):
+        self.timestamp[0] = 1 if self.timestamp[0] == 0 else 0
+
+        timestamp_val = (self.timestamp[1] << 8) | self.timestamp[0] # bitshifting
+        timestamp_bytes = struct.pack(">H", timestamp_val) # big-endian gömülü kodundan dolayı
+
+        process_id_bytes = struct.pack(">H", 0x0020) # IMU data process id (binary 0000 0000 0010 0000), yine big-endian 2 byte unsigned short
+
+        request_msg = timestamp_bytes + process_id_bytes
+
+        self.sock.sendto(request_msg, (REMOTE_IP, REMOTE_PORT))
+
     def parse_and_create(self, data):
         parsed_data = struct.unpack("<16f", data)
 
@@ -53,9 +70,16 @@ class imu_udp():
     def run(self):
         while not rospy.is_shutdown():
             try:
-                data, addr = self.sock.recvfrom(1024)  # buffer size
+                self.request_data()
+                data, addr = self.sock.recvfrom(512)  # buffer size
                 imu_msg = self.parse_and_create(data)
                 self.pub.publish(imu_msg)
+                rospy.loginfo(f"Published IMU data/\nAccel({imu_msg.linear_acceleration.x:.3f}, {imu_msg.linear_acceleration.y:.3f}, {imu_msg.linear_acceleration.z:.3f})\n "
+                              f"Gyro({imu_msg.angular_velocity.x:.3f}, {imu_msg.angular_velocity.y:.3f}, {imu_msg.angular_velocity.z:.3f})\n "
+                              f"Orientation({imu_msg.orientation.w:.3f}, {imu_msg.orientation.x:.3f}, {imu_msg.orientation.y:.3f}, {imu_msg.orientation.z:.3f})\n")
+                
+            except socket.timeout:
+                rospy.loginfo("did not receive a response from the IMU in time.")
 
             except Exception as e:
                 rospy.loginfo(f"error receiving or parsing data: {e}")
@@ -67,7 +91,4 @@ if __name__ == "__main__":
         imu_listener.run()
     
     except rospy.ROSInterruptException:
-        rospy.loginfo("shutting down imu_udp_node.")
-
-
-
+        rospy.signal_shutdown("shutting down imu_udp_node.")
